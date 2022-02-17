1939f63de2cf75fd07d09faae7c4d17c425abecb
Crowdbotics <team@crowdbotics.com>
2022-02-17 21:58:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/spacetime_infinity_33684/MainActivity.java
@@ -0,0 +1,15 @@
+package com.spacetime_infinity_33684;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "spacetime_infinity_33684";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spacetime_infinity_33684",
+  url: "https://spacetime_infinity_33684.botics.co",
+  api: "https://spacetime_infinity_33684.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
